b3a86e5c2b9799cfe9644682ba36bb28fd317d80
Locked_Fog <Aboxyxy@outlook.com>
2025-12-04 12:21:23 +0800

    Linter

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     kotlin("jvm") version "2.2.0"
     kotlin("plugin.serialization") version "2.2.0"
     id("maven-publish")
+
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
 group = "com.github.locked-fog"
@@ -48,4 +50,19 @@ publishing {
             version = "0.3.2"
         }
     }
+}
+
+detekt {
+    // 指定仅分析 src 目录（跳过 test 或 build 目录）
+    source.setFrom("src/main/kotlin")
+
+    // 自动修复格式问题（类似 ktlint 的功能）
+    autoCorrect = true
+
+    // 指定配置文件（稍后创建）
+    config.setFrom("config/detekt/detekt.yml")
+
+    // 只要发现任何一个问题，就让构建失败（零容忍策略）
+    // 如果是老项目，可以设置为 false，只看报告不挂流水线
+    buildUponDefaultConfig = true
 }
